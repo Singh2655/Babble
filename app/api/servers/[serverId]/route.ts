a87e5eae34a7b1f35ddc460e6d78a79b12687413
Singh2655 <115347390+Singh2655@users.noreply.github.com>
2024-01-20 19:47:00 +0530

    added server delete and leave function

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,26 +1,50 @@
-import { currentProfile } from "@/lib/currentProfile"
-import { db } from "@/lib/db"
-import { NextResponse } from "next/server"
+import { currentProfile } from "@/lib/currentProfile";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
 
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+    if (!params.serverId)
+      return new NextResponse("Server ID is missing", { status: 400 });
+    const server = await db.server.delete({
+        where:{
+            id:params.serverId,
+            profileId:profile.id
+        }
+    });
+    return NextResponse.json(server)
+  } catch (error) {
+    console.log("[SERVER_DELETE_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
 
-export async function PATCH(req:Request,{params}:{params:{serverId:string}}){
-    try {
-        const {name,imageUrl}=await req.json()
-        const profile=await currentProfile()
-        if(!profile)return new NextResponse("Unauthorized",{status:401})
-        const server=await db.server.update({
-             where:{
-               id:params.serverId,
-                profileId:profile.id
-             },
-             data:{
-                name,
-                imageUrl,
-             },
-        })
-        return NextResponse.json(server)
-    } catch (error) {
-        console.log("[SERVER_ID_PATCH]",error)
-        return new NextResponse("Internal error",{status:500})
-    }
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const { name, imageUrl } = await req.json();
+    const profile = await currentProfile();
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+      data: {
+        name,
+        imageUrl,
+      },
+    });
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_PATCH]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
 }
--- components/server/ServerHeader.tsx
+++ components/server/ServerHeader.tsx
@@ -47,7 +47,7 @@ const ServerHeader = ({server,role}:ServerHeaderProps) => {
                     </DropdownMenuItem>
             )}
             {isModerator && (
-                <DropdownMenuItem className='px-3 py-2 cursor-pointer text-sm'>
+                <DropdownMenuItem className='px-3 py-2 cursor-pointer text-sm' onClick={()=>onOpen("createChannel")}>
                     Create Channel
                     <PlusCircle className='h-4 w-4 ml-auto'/>
                     </DropdownMenuItem>
@@ -56,13 +56,13 @@ const ServerHeader = ({server,role}:ServerHeaderProps) => {
                 <DropdownMenuSeparator/>
             )}
             {isAdmin && (
-                <DropdownMenuItem className='text-rose-500 px-3 py-2 cursor-pointer text-sm'>
+                <DropdownMenuItem className='text-rose-500 px-3 py-2 cursor-pointer text-sm' onClick={()=>onOpen("deleteServer",{server})}>
                     Delete Server
                     <Trash className='h-4 w-4 ml-auto'/>
                     </DropdownMenuItem>
             )}
             {!isAdmin && (
-                <DropdownMenuItem className='text-rose-500 px-3 py-2 cursor-pointer text-sm'>
+                <DropdownMenuItem className='text-rose-500 px-3 py-2 cursor-pointer text-sm' onClick={()=>onOpen("leaveServer",{server})}>
                     Leave Server
                     <LogOut className='h-4 w-4 ml-auto'/>
                     </DropdownMenuItem>
