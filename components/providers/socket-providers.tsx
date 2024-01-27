887b5b047c170e1f6037f60bff204e33f3404f2a
Singh2655 <115347390+Singh2655@users.noreply.github.com>
2024-01-27 10:49:36 +0530

    final

--- components/providers/socket-providers.tsx
+++ components/providers/socket-providers.tsx
@@ -22,7 +22,7 @@ export const SocketProvider=({children}:{children:React.ReactNode})=>{
     const [socket,setSocket]=useState(null)
     const [isConnected, setIsConnected] = useState(false)
     useEffect(()=>{
-        const socketInstance=new (ClientIO as any)(process.env.NEXT_PUBLI_SITE_URL!,{
+        const socketInstance=new (ClientIO as any)(process.env.NEXT_PUBLIC_SITE_URL!,{
             path:'/api/socket/io',
             addTrailingSlash:false
         })
